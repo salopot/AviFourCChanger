409dd2aa90538efe2703c927e10cecbdee6c1d30
salopot <salopot@gmail.com>
2022-10-21 11:28:06 +0300

    Add directory processing

--- main.go
+++ main.go
@@ -2,47 +2,105 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-func main() {
-	inputPath := os.Args[1]
-	fmt.Println(inputPath)
+const containerExt = "avi"
 
-	var changeMap = map[string]string{
-		"DIVX": "FMP4",
-		"XVID": "FMP4",
-		"DIV5": "FMP4",
-		"DIV3": "MP43",
-	}
-
-	updateFourCC(inputPath, changeMap)
+var changeMap = map[string]string{
+	"DIVX": "FMP4",
+	"XVID": "FMP4",
+	"DIV5": "FMP4",
+	"DIV3": "MP43",
 }
 
-func updateFourCC(name string, changeMap map[string]string) {
+func main() {
+	inputPath := os.Args[1]
+
+	files, err := listFiles(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		err = updateFourCC(file, changeMap)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func listFiles(path string) ([]string, error) {
+	var files []string
+	fi, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("%s: no such file or directory", path)
+		}
+		return nil, err
+	}
+	if fi.IsDir() {
+		err := filepath.WalkDir(path, func(path string, dirEntry fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if !dirEntry.IsDir() {
+				if matchFileType(dirEntry.Name()) {
+					files = append(files, path)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+	} else if matchFileType(fi.Name()) {
+		files = append(files, path)
+	}
+
+	return files, nil
+}
+
+func matchFileType(name string) bool {
+	return strings.EqualFold(filepath.Ext(name), "."+containerExt)
+}
+
+func updateFourCC(path string, changeMap map[string]string) error {
 	const fourccHeaderOffset = 112
 	const fourccDescriptionOffset = 188
 
-	file, err := os.OpenFile(name, os.O_RDWR, 0644)
+	file, err := os.OpenFile(path, os.O_RDWR, 0644)
 	if err != nil {
-		log.Fatalf("failed opening file: %s", err)
+		return err
 	}
 	defer file.Close()
 
 	buff := make([]byte, 4)
-	file.ReadAt(buff, fourccDescriptionOffset)
+	_, err = file.ReadAt(buff, fourccDescriptionOffset)
+	if err != nil {
+		return fmt.Errorf("%s: can't read FourCC with error %s", path, err)
+	}
 	fourCCDescription := string(buff)
-
 	if replace, found := changeMap[fourCCDescription]; found {
-		fmt.Printf("Codec %s replcacing to %s...", fourCCDescription, replace)
+		fmt.Printf("FourCC %s replacing to %s...", fourCCDescription, replace)
 
-		file.WriteAt([]byte(strings.ToLower(replace)), fourccHeaderOffset)
-		file.WriteAt([]byte(replace), fourccDescriptionOffset)
+		_, err = file.WriteAt([]byte(strings.ToLower(replace)), fourccHeaderOffset)
+		if err != nil {
+			fmt.Println("Error")
+			return fmt.Errorf("%s: can't update FourCC with error %s", path, err)
+		}
+		_, err = file.WriteAt([]byte(replace), fourccDescriptionOffset)
+		if err != nil {
+			fmt.Println("Error")
+			return fmt.Errorf("%s: can't update FourCC with error %s", path, err)
+		}
 
 		fmt.Println("OK")
 	} else {
-		fmt.Printf("Codec %s skipped\n", fourCCDescription)
+		fmt.Printf("FourCC %s skipped\n", fourCCDescription)
 	}
+	return nil
 }
